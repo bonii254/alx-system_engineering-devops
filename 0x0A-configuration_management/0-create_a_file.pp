a5eac957c6e970971520d80b31d54f03e13e4bc0
bonface murangiri <bonnyrangi95@gmail.com>
2023-11-25 01:28:27 +0000

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file in /tmp. with specified requirements:
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
