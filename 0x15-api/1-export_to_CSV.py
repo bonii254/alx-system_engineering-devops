4c8064fd994de8518bcece376d2a188df5a61e56
bonface murangiri <bonnyrangi95@gmail.com>
2024-01-04 17:06:25 +0000

    csv and json

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,17 +5,15 @@ import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    username = user.get("name")
 
-    with open("{}.csv".format(user_id), "w") as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for t in todos:
-            csv_writer.writerow(
-                [user_id, username, t.get("completed"), t.get("title")])
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,14 +9,13 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    username = user.get("name")
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
-            "task": t.get("title"),
-            "completed": t.get("completed"),
-            "username": username,
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
             } for t in todos]}, jsonfile)
